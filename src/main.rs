043e75ad1ac92b1d973e649cfad7127215a60f4d
Étienne Besançon <etienne.besancon@pollen-robotics.com>
2023-09-19 11:44:35 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -2,84 +2,60 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+//use core::fmt::Write;
+use core::str::from_utf8;
+
 use defmt::*;
 use embassy_executor::Spawner;
-// use embassy_stm32::dma::NoDma;
-use embassy_stm32::gpio::{AnyPin, Level, Output, Speed};
-use embassy_stm32::peripherals::{DMA1_CH0, DMA1_CH1, USART1};
-use embassy_stm32::usart::{Config, Uart, UartRx, UartTx};
-use embassy_stm32::{bind_interrupts, peripherals, usart};
-use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
-use embassy_sync::channel::Channel;
-// use embassy_time::{Duration, Timer};
-// use rustypot::protocol::V1; //unfortunately, we are not ready for that yet ;(
-
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
+use embassy_stm32::dma::NoDma;
+//use embassy_stm32::peripherals::SPI4;
+use embassy_stm32::time::mhz;
+use embassy_stm32::{spi, Config};
 use {defmt_rtt as _, panic_probe as _};
 
-static RX_CHANNEL: Channel<ThreadModeRawMutex, [u8; 6], 1> = Channel::new();
-static TX_CHANNEL: Channel<ThreadModeRawMutex, [u8; 6], 1> = Channel::new();
-
-bind_interrupts!(struct Irqs {
-    USART1 => usart::InterruptHandler<peripherals::USART1>;
-});
-
-#[embassy_executor::task]
-async fn writer(mut usart: UartTx<'static, USART1, DMA1_CH0>, dir_pin: AnyPin) {
-    let mut dir = Output::new(dir_pin, Level::High, Speed::High);
-
-    loop {
-        let buf = TX_CHANNEL.receive().await;
-        dir.set_high(); //TODO: high or low?
-        usart.write(&buf).await.ok();
-        dir.set_low();
-    }
-}
-
-#[embassy_executor::task]
-async fn reader(mut rx: UartRx<'static, USART1, DMA1_CH1>) {
-    let mut buf = [0; 6];
-    loop {
-        info!("reading...");
-        unwrap!(rx.read(&mut buf).await);
-        RX_CHANNEL.send(buf).await;
-    }
-}
-
 #[embassy_executor::main]
-async fn main(spawner: Spawner) -> ! {
-    let p = embassy_stm32::init(Default::default());
+async fn main(_spawner: Spawner) {
+    info!("Hello World!");
 
-    // let mut dir = Output::new(p.PD9, Level::High, Speed::High);
-    let config = Config::default();
-    let usart = Uart::new(
-        p.USART1, p.PB15, p.PB14, Irqs, p.DMA1_CH0, p.DMA1_CH1, config,
-    );
+    let mut config = Config::default();
+    config.rcc.sys_ck = Some(mhz(400));
+    config.rcc.hclk = Some(mhz(200));
+    config.rcc.pll1.q_ck = Some(mhz(100));
+    let p = embassy_stm32::init(config);
 
-    // unwrap!(usart.blocking_write(b"Type 8 chars to echo!\r\n"));
+    let mut led_hello = Output::new(p.PC9, Level::High, Speed::Low);
+    let mut led_error = Output::new(p.PC8, Level::High, Speed::Low);
+    led_error.set_low();
+   
+    let mut spi_config = spi::Config::default();
+    spi_config.frequency = mhz(1);
 
-    let (tx, rx) = usart.split();
-
-    unwrap!(spawner.spawn(reader(rx)));
-    unwrap!(spawner.spawn(writer(tx, p.PD9.into())));
+    let mut spi = spi::Spi::new(p.SPI4, p.PE12, p.PE6, p.PE5, NoDma, NoDma, spi_config);
+    let mut spi_driver_cs = Output::new(p.PC15, Level::High, Speed::Low);
+    spi_driver_cs.set_high();
+    let mut spi_foc_cs = Output::new(p.PE3, Level::High, Speed::Low);
+    spi_foc_cs.set_high();
 
     loop {
-        let buf = RX_CHANNEL.receive().await;
-        let mut pp = [0xff, 0xff, 0x2A, 0x02, 0x01, 0xff];
-        let crc = 0x2A + 0x02 + 0x01;
-        pp[5] = !crc;
+        led_hello.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+        led_hello.set_low();
 
-        /////////TODO
-        // let bytes = vec![0x00];
-        // let sp = StatusPacketV1::from_bytes(&bytes, 42);
+        // 40 bits (rw + addr 7 + data 32)
+        //let mut data = [0x80u8, 0x00u8, 0x00u8, 0x00u8, 0b00000100u8];
+//        let mut data = [0x55u8, 0x00u8, 0x00u8, 0x00u8, 0b10101010u8];
+        let mut data = [0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x04u8];
 
-        if pp == buf {
-            info!("Answering to ping");
-            let mut sp = [0xff, 0xff, 0x2A, 0x02, 0x00, 0xff];
-            let crc_s = 0x2A + 0x02;
-            sp[5] = !crc_s;
-            // TX_CHANNEL.send(buf).await;
-            TX_CHANNEL.send(sp).await;
-            // unwrap!(tx.write(&buf).await);
+        spi_driver_cs.set_low();
+        let result = spi.blocking_transfer_in_place(&mut data);
+        if let Err(_) = result {
+            defmt::panic!("crap");
         }
+        spi_driver_cs.set_high();
+        info!("read via spi: {}", from_utf8(&data).unwrap());
+        Timer::after(Duration::from_millis(5000)).await;
+
     }
 }
