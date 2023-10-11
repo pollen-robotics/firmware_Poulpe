0177bb6d4761d8c05b2668b8a3a1ba1144fd4595
Étienne Besançon <etienne.besancon@pollen-robotics.com>
2023-10-11 11:24:47 +0200

    Now using lib to speak with TMCxxx.

--- src/main.rs
+++ src/main.rs
@@ -15,64 +15,7 @@ use embassy_stm32::peripherals::PE3;
 use {defmt_rtt as _, panic_probe as _};
 
 mod tmc6200;
-
-fn tmc4671_transmit_raw_data(
-    write_bit: bool,
-    addr: u8,
-    data: &mut u32,
-    per: &mut Spi<'_, SPI4, NoDma, NoDma>,
-    cs: &mut Output<'_, PE3>,
-) -> Result<[u8; 4], embassy_stm32::spi::Error> {
-    // Building array
-    let mut msb_data = addr;
-    if write_bit == true {
-        msb_data = addr | 0b10000000;
-    }
-    let data_u8_array = data.to_le_bytes();
-    let mut transfer_data = [msb_data, data_u8_array[0], data_u8_array[1], data_u8_array[2], data_u8_array[3]];
-
-    // Sending data
-    cs.set_low();
-    let result = per.blocking_transfer_in_place(&mut transfer_data); // Todo: the error is not treated.
-    cs.set_high();
-
-    let mut read_data = [0x00u8; 4];
-    read_data[0] = transfer_data[1];
-    read_data[1] = transfer_data[2];
-    read_data[2] = transfer_data[3];
-    read_data[3] = transfer_data[4];
-
-    Ok(read_data)
-}
-
-fn tmc6200_transmit_raw_data(
-    write_bit: bool,
-    addr: u8,
-    data: &mut u32,
-    per: &mut Spi<'_, SPI4, NoDma, NoDma>,
-    cs: &mut Output<'_, PC15>,
-) -> Result<[u8; 4], embassy_stm32::spi::Error> {
-    // Building array
-    let mut msb_data = addr;
-    if write_bit == true {
-        msb_data = addr | 0b10000000;
-    }
-    let data_u8_array = data.to_le_bytes();
-    let mut transfer_data = [msb_data, data_u8_array[0], data_u8_array[1], data_u8_array[2], data_u8_array[3]];
-
-    // Sending data
-    cs.set_low();
-    let result = per.blocking_transfer_in_place(&mut transfer_data); // Todo: the error is not treated.
-    cs.set_high();
-
-    let mut read_data = [0x00u8; 4];
-    read_data[0] = transfer_data[1];
-    read_data[1] = transfer_data[2];
-    read_data[2] = transfer_data[3];
-    read_data[3] = transfer_data[4];
-
-    Ok(read_data)
-}
+mod tmc4671;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -89,61 +32,23 @@ async fn main(_spawner: Spawner) {
     let mut spi_config = spi::Config::default();
     spi_config.mode = spi::MODE_3;
 
-    // Uses J5 / SPI4
-    let mut spi_j5_ventouse = spi::Spi::new(p.SPI4, p.PE12, p.PE6, p.PE5, NoDma, NoDma, spi_config);
-    let mut spi_driver_cs = Output::new(p.PC15, Level::High, Speed::Low);
-    spi_driver_cs.set_high();
-    let mut spi_foc_cs = Output::new(p.PE3, Level::High, Speed::Low);
-    spi_foc_cs.set_high();
+    // Configure SPI/CS (J5 / SPI4)
+//    let mut driver_a = tmc6200::TMC6200::new(p.PC15, p.PE12, p.PE5, p.PE6, p.SPI4, NoDma, NoDma);
+    let mut foc_a = tmc4671::TMC4761::new(p.PE3, p.PE12, p.PE5, p.PE6, p.SPI4, NoDma, NoDma);
 
     let write_b = false;
-    let reg_addr = 0x00u8;
+    let reg_addr = 0x00u8; // Read Chip info data
     let mut data = 0x00000000u32;
-    let result = tmc6200_transmit_raw_data(write_b, reg_addr, &mut data, &mut spi_j5_ventouse, &mut spi_driver_cs);
-    if let Err(_) = result {
+    let res = foc_a.transmit_raw_data(write_b, reg_addr, &mut data);
+    if let Err(_) = res {
         defmt::panic!("crap_from_fn!");
     }
-    info!("read GCONF from fn: {:#04x}", result.unwrap());
+    info!("read GCONF from fn: {:#04x}", res.unwrap());
     Timer::after(Duration::from_millis(1000)).await;
-
-    let write_b = true;
-    let reg_addr = 0x00u8;
-    let mut data = 0x00000000u32; // Switch to single line (aka 6PWM)
-    let result = tmc6200_transmit_raw_data(write_b, reg_addr, &mut data, &mut spi_j5_ventouse, &mut spi_driver_cs);
-    if let Err(_) = result {
-        defmt::panic!("crap_from_fn!");
-    }
-    info!("write GCONF from fn: {:#04x}", result.unwrap());
-    Timer::after(Duration::from_millis(1000)).await;
-
-    let write_b = false;
-    let reg_addr = 0x00u8;
-    let mut data = 0x00000000u32;
-    let result = tmc6200_transmit_raw_data(write_b, reg_addr, &mut data, &mut spi_j5_ventouse, &mut spi_driver_cs);
-    if let Err(_) = result {
-        defmt::panic!("crap_from_fn!");
-    }
-    info!("read GCONF from fn: {:#04x}", result.unwrap());
-    Timer::after(Duration::from_millis(1000)).await;
-
-
-    info!("Set foc_b_enable");
-    let mut foc_b_enable = Output::new(p.PE0, Level::High, Speed::Low);
-    foc_b_enable.set_high();
 
     loop {
         led_hello.set_high();
         Timer::after(Duration::from_millis(500)).await;
-
-        // Check TMC4761
-        let write_b = false;
-        let reg_addr = 0x00u8; // Read Chip info data
-        let mut data = 0x00000000u32;
-        let result = tmc4671_transmit_raw_data(write_b, reg_addr, &mut data, &mut spi_j5_ventouse, &mut spi_foc_cs);
-        if let Err(_) = result {
-            defmt::panic!("crap_from_fn!");
-        }
-        info!("read from fn: {:#04x}", result.unwrap());
 
         led_hello.set_low();
         Timer::after(Duration::from_millis(500)).await;
