64bf25388c2c401e8e53d86b6241a861ce1dd52b
gospar <antun.skuric@outlook.com>
2025-01-15 15:59:17 +0100

    added the SDO for starting the firmware update

--- src/ethercat/task1.rs
+++ src/ethercat/task1.rs
@@ -12,7 +12,7 @@ use embassy_stm32::{
     gpio::{Level, Output, Speed},
 };
 
-use crate::ethercat::lan9252::{Lan9252, Lan9252Registers, AL_STATUS, WDOG_STATUS, READY};
+use crate::ethercat::lan9252::*;
 use crate::state_machine::poulpe_state::PoulpeState;
 use embassy_sync::blocking_mutex::{raw::NoopRawMutex, Mutex};
 use embassy_time::{Duration, Instant, Timer};
@@ -28,6 +28,8 @@ use embassy_boot_stm32::{AlignedBuffer, BlockingFirmwareUpdater, FirmwareUpdater
 use embassy_stm32::flash::{Flash, WRITE_SIZE};
 use embedded_storage::nor_flash::NorFlash;
 use embassy_boot_stm32::State;
+
+use super::lan9252::{self, ESM_PREOP};
 
 // the addresses of the motors in the LAN9252 memory
 pub enum Lan9252Memory {
@@ -146,20 +148,6 @@ pub async fn messsage_handler(ethconf: LAN9252Config, spi_config: spi::Config, f
     }
     Timer::after(Duration::from_micros(1)).await;
 
-    // before we start the loop write zeros to the OrbitaIn memory
-    // to avoid old data being read
-    debug!("Reinitialise the OrbitaIn memory!");
-    let data = [0; 1 + 12 * N_AXIS];
-    match lan9252
-        .write_bytes(&data, Lan9252Memory::OrbitaIn as u16)
-        .await
-    {
-        Ok(_) => {}
-        Err(e) => {
-            error!("Write data error! {:?}", e)
-        }
-    }
-
     // the vatiables to keep track of the watchdog counter
     let mut last_watchdog_counter = 0;
     let mut last_watchdog_counter_timestamp = Instant::now();
@@ -185,8 +173,25 @@ pub async fn messsage_handler(ethconf: LAN9252Config, spi_config: spi::Config, f
     let mut data_in_buf: usize  = 0;
     let mut written_bytes: u32 = 0;
     let mut firmware_update_done: bool = false;
+
     loop {
 
+        // // check if the lan9252 is in boot mode
+        // match lan9252.read_register_indirect(AL_STATUS, 1).await {
+        //     Ok(al) => {
+        //         debug!("Status: {:#x}", al[0] & 0x0F);
+        //         if al[0] != ESM_BOOT{
+        //             warn!("LAN9252 is not in boot mode!");
+        //             Timer::after(Duration::from_millis(1000)).await;
+        //             continue;
+        //         }
+        //     }
+        //     Err(e) => {
+        //         error!("Read status error {:?}", e)
+        //     }
+        // }
+
+        Timer::after(Duration::from_micros(1)).await;
         
         // check if the master if the master is connected 
         // by checking the watchdog status
@@ -210,7 +215,7 @@ pub async fn messsage_handler(ethconf: LAN9252Config, spi_config: spi::Config, f
             .await
             {
                 Ok(_) => {
-                    info!("OrbitaIn: {:?}", data_copy);
+                    debug!("OrbitaIn: {:?}", data_copy);
                 }
                 Err(e) => {
                     error!("Read data error! {:?}", e);
@@ -219,84 +224,131 @@ pub async fn messsage_handler(ethconf: LAN9252Config, spi_config: spi::Config, f
 
             let data = data_copy.clone();
 
+
             // let datagram_header: [u8; 6] = data[0..6].try_into().unwrap();
             // let mbox_header: [u8; 6] = data[6..12].try_into().unwrap();
 
             // parse the datagram 
-            if data[5] != 0x4 {
-                error!("Wrong FoE protocol!");
-                continue;
-            }
+            if data[5] == 3{
+                info!("CoE protocol received: {:?}", data[5]);
 
+                info!("Data: {:?}", data);
+                let address = u16::from_le_bytes(data[9..11].try_into().unwrap());
+                let sub_index = data[11];
+                let data_rec = u32::from_le_bytes(data[12..16].try_into().unwrap());
+                info!("Address: {:?}, subindex: {:?}, data: {:?}", address, sub_index, data_rec);
 
-            if data[6] == 0x2 {
-                info!("New file write request!");
-                let filename_size = u16::from_le_bytes(data[0..2].try_into().unwrap()) - 6;
-                let file_name_str = &data[12..12+filename_size as usize];
-                match str::from_utf8(file_name_str.try_into().unwrap()){
-                    Ok(string) => {
-                        info!("File name: {}", string);
-                    },
+                if address == 0x100 && sub_index == 0x01{
+                    info!("Firmware upload end command!");
+                    let firmware_upload_size = data_rec;
+                    if firmware_upload_size == 0{
+                        error!("Firmware upload size is 0!");
+                    }else if no_received_bytes == 0 {
+                        error!("No data received yet!");
+                    }else if firmware_upload_size == no_received_bytes {
+                        firmware_update_done = true;
+                        info!("Received file size: {:?}, expected: {:?}", no_received_bytes, firmware_upload_size);
+                        info!("Proceeding to firmware update!");
+                    }else{
+                        error!("Received file size: {:?}, expected: {:?}", no_received_bytes, firmware_upload_size);
+                    }
+                }
+                    
+                // // construct the datagram to send to the master
+                
+
+                let mut data_write = [0x00u8; 128];
+                // construct the datagram to acknowledge the data received
+                data_write[0] = 10; // data length
+                data_write[5] = 0x03; // set the CoE protocol
+                let data_start_ind:usize = 6;
+                data_write[data_start_ind..data_start_ind+2].copy_from_slice(&u16::to_le_bytes(12288));
+                data_write[data_start_ind+2] = 0x60;
+                data_write[data_start_ind+3..data_start_ind+5].copy_from_slice(&u16::to_le_bytes(address));
+                data_write[data_start_ind+5] = sub_index;
+
+                Timer::after(Duration::from_millis(1)).await;
+                // heder is 6 bytes and then the 
+                match lan9252.write_bytes_large(&data_write, Lan9252Memory::OrbitaInAck as u16).await {
+                    Ok(_) => {}
                     Err(e) => {
-                        error!("Error parsing file name: {:?}", file_name_str);
+                        error!("Write data error! {:?}", e)
                     }
                 }
-                no_received_bytes = 0; 
-                buf = AlignedBuffer([0; 4096]);
-                data_in_buf = 0;
-                written_bytes = 0;
-            }else if data[6] == 0x3 {
-                let data_chunk_lenght = u16::from_le_bytes(data[0..2].try_into().unwrap()) - 6;
 
-
-                if data_in_buf + (data_chunk_lenght as usize) > 4096 {
-                    let rest_in_buf = 4096 - data_in_buf;
-                    buf.as_mut()[data_in_buf..4096].copy_from_slice(data[12..(12 + rest_in_buf) as usize].as_ref());
-                    writer.write(written_bytes, buf.as_ref()).unwrap();
-                    written_bytes = written_bytes + 4096;
-                    buf.as_mut().fill(255);
-                    buf.as_mut()[0..(data_chunk_lenght as usize - rest_in_buf)].copy_from_slice(data[(12 + rest_in_buf) as usize..(12 + data_chunk_lenght) as usize].as_ref());
-                    data_in_buf = data_chunk_lenght as usize - rest_in_buf;
-                }
-                else{
-                    buf.as_mut()[data_in_buf..(data_in_buf + data_chunk_lenght as usize)].copy_from_slice(data[12..(12 + data_chunk_lenght) as usize].as_ref());
-                    data_in_buf = data_in_buf + data_chunk_lenght as usize;
-                }
-                no_received_bytes = no_received_bytes + data_chunk_lenght as u32;
-                info!("Received data in bytes: {:?}B, written bytes: {:?}B", no_received_bytes, written_bytes);
-                info!("Data chunk length: {:?}", data_chunk_lenght);
-                if data_chunk_lenght < 116 {
-                    if data_in_buf > 0 {
-                        writer.write(written_bytes, &buf.as_ref()[0..4096]).unwrap();
-                        written_bytes = written_bytes + data_in_buf as u32;
+            }else if data[5] == 4 {
+                info!("FoE protocol received: {:?}", data[5]);
+                if data[6] == 0x2 {
+                    info!("New file write request!");
+                    let filename_size = u16::from_le_bytes(data[0..2].try_into().unwrap()) - 6;
+                    let file_name_str = &data[12..12+filename_size as usize];
+                    match str::from_utf8(file_name_str.try_into().unwrap()){
+                        Ok(string) => {
+                            info!("File name: {}", string);
+                        },
+                        Err(e) => {
+                            error!("Error parsing file name: {:?}", file_name_str);
+                        }
                     }
-                    firmware_update_done = true;
-                    info!("End of file received!");
+                    no_received_bytes = 0; 
+                    buf = AlignedBuffer([0; 4096]);
+                    data_in_buf = 0;
+                    written_bytes = 0;
+                }else if data[6] == 0x3 {
+                    let data_chunk_lenght = u16::from_le_bytes(data[0..2].try_into().unwrap()) - 6;
+
+
+                    if data_in_buf + (data_chunk_lenght as usize) > 4096 {
+                        let rest_in_buf = 4096 - data_in_buf;
+                        buf.as_mut()[data_in_buf..4096].copy_from_slice(data[12..(12 + rest_in_buf) as usize].as_ref());
+                        writer.write(written_bytes, buf.as_ref()).unwrap();
+                        written_bytes = written_bytes + 4096;
+                        buf.as_mut().fill(255);
+                        buf.as_mut()[0..(data_chunk_lenght as usize - rest_in_buf)].copy_from_slice(data[(12 + rest_in_buf) as usize..(12 + data_chunk_lenght) as usize].as_ref());
+                        data_in_buf = data_chunk_lenght as usize - rest_in_buf;
+                    }
+                    else{
+                        buf.as_mut()[data_in_buf..(data_in_buf + data_chunk_lenght as usize)].copy_from_slice(data[12..(12 + data_chunk_lenght) as usize].as_ref());
+                        data_in_buf = data_in_buf + data_chunk_lenght as usize;
+                    }
+                    no_received_bytes = no_received_bytes + data_chunk_lenght as u32;
+                    info!("Received data in bytes: {:?}B, written bytes: {:?}B", no_received_bytes, written_bytes);
+                    info!("Data chunk length: {:?}", data_chunk_lenght);
+                    if data_chunk_lenght < 116 {
+                        if data_in_buf > 0 {
+                            writer.write(written_bytes, &buf.as_ref()[0..4096]).unwrap();
+                            written_bytes = written_bytes + data_in_buf as u32;
+                        }
+                        info!("End of file received!");
+                    }
+                }else{
+                    error!("Unknown command! {:?}", data[6]);
+                }
+
+                let mut data_write = [0x00u8; 128];
+                // construct the datagram to send to the master
+                data_write[5] = 0x04; // set the FoE protocol
+
+                data_write[0] = 0x01; // set the data size to 1
+                data_write[6] = 0x04; // acknowledge the data received
+
+                // heder is 6 bytes and then the 
+                match lan9252.write_bytes_large(&data_write, Lan9252Memory::OrbitaInAck as u16).await {
+                    Ok(_) => {info!("Data ack sent!");}
+                    Err(e) => {
+                        error!("Write data error! {:?}", e)
+                    }
                 }
             }else{
-                error!("Unknown command! {:?}", data[6]);
+                error!("Wrong protocol! Protocol: {}", data[5]);
             }
 
             // a a microsecond delay to avoid reading the same data
             Timer::after(Duration::from_micros(1)).await;
-
-            let mut data_write = [0x00u8; 128];
-
-            // construct the datagram to send to the master
-            data_write[5] = 0x04; // set the FoE protocol
-
-            data_write[0] = 0x01; // set the data size to 1
-            data_write[6] = 0x04; // acknowledge the data received
     
-            // heder is 6 bytes and then the 
-            match lan9252.write_bytes_large(&data_write, Lan9252Memory::OrbitaInAck as u16).await {
-                Ok(_) => {}
-                Err(e) => {
-                    error!("Write data error! {:?}", e)
-                }
-            }
 
             if firmware_update_done {
+                firmware_update_done= false;
                 updater.mark_updated().unwrap();
                 Timer::after(Duration::from_millis(1000)).await;
                 info!("Rebooting the device!");
